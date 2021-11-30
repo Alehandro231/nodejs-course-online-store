a23d67d3f3dcbba9effb9e0fad9ce362577f3df4
Aleksey Efimovich <aefimovich@appliedtech.ru>
2021-12-01 01:49:28 +0500

    подготовлена сборка для деплоя на сервер

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://aleksey:JSXBBCbg6sJ9nU6@cluster0.iw2je.mongodb.net/shop',
-  SESSION_SECRET: 'secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'video.kursi.word.pwp@gmail.com',
-  BASE_URL: 'http://localhost:3000',
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
